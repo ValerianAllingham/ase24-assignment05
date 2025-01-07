199628d96d71d31ac0b9e831d64c7ef23ad664ec
Valerian Allingham <Valerian.allingham123@gmail.com>
2025-01-07 14:57:01 +0100

    add: Completed UserController and UserSerivce TODOs

--- api/src/main/java/de/unibayreuth/se/taskboard/api/controller/UserController.java
+++ api/src/main/java/de/unibayreuth/se/taskboard/api/controller/UserController.java
@@ -4,8 +4,13 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import de.unibayreuth.se.taskboard.business.ports.UserService;
+import de.unibayreuth.se.taskboard.business.domain.User;
+
+import java.util.List;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -14,11 +19,34 @@ import org.springframework.web.bind.annotation.*;
         )
 )
 @Tag(name = "Users")
-@Controller
+@RestController // Change to @RestController since it's handling REST endpoints
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
     // TODO: Add GET /api/users endpoint to retrieve all users.
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+
+
     // TODO: Add GET /api/users/{id} endpoint to retrieve a user by ID.
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable String id) {
+        return userService.getUserById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
     // TODO: Add POST /api/users endpoint to create a new user based on a provided user DTO.
+    @PostMapping
+    public ResponseEntity<Void> createUser(@RequestBody User user) {
+        userService.createUser(user);
+        return ResponseEntity.status(201).build(); // Return HTTP 201 (Created)
+    }
 }
--- business/src/main/java/de/unibayreuth/se/taskboard/business/ports/UserService.java
+++ business/src/main/java/de/unibayreuth/se/taskboard/business/ports/UserService.java
@@ -1,6 +1,17 @@
 package de.unibayreuth.se.taskboard.business.ports;
 
+import java.util.List;
+import java.util.Optional;
+import de.unibayreuth.se.taskboard.business.domain.User;
+
 public interface UserService {
+   
+
+   List<User> getAllUsers();
+
+   Optional<User> getUserById(String id);
+   
+   void createUser(User user);
     //TODO: Add user service interface that the controller uses to interact with the business layer.
     //TODO: Implement the user service interface in the business layer, using the existing user persistence service.
-}
+}  
